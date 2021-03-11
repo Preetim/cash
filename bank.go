448826caa00bc4b393e7688278672c5abf9aacbf
Preeti Mishra <preetimishra@Preeti-Mishra.local>
2021-03-11 14:17:29 +1100

    Add basic withdraw and test

--- bank.go
+++ bank.go
@@ -1,7 +1,5 @@
 package cash
 
-import "fmt"
-
 type transaction struct {
 	transactionType string
 	amount int
@@ -28,16 +26,25 @@ func deposit(account account, amount int) transaction {
 	return t
 }
 
-func findBankBalance(transactions []transaction) int {
-	bankBalance = 0
-	fmt.Println(transactions)
-	for k, v := range transactions {
-		fmt.Printf("Key is %d\n", k)
-		bankBalance += v.amount
-	}
-	fmt.Printf("bankBalance is %d\n", bankBalance)
-	return bankBalance
+//func findBankBalance(transactions []transaction) int {
+//	bankBalance = 0
+//	for k, v := range transactions.account {
+//		//fmt.Printf("Key is %d\n", k)
+//		bankBalance += v.amount
+//	}
+//	fmt.Printf("bankBalance is %d\n", bankBalance)
+//	return bankBalance
+//}
 
+func withdraw(account account, amount int) transaction {
+	account.balance -= amount
+	t := transaction{
+		transactionType: "withdraw",
+		amount: amount,
+		account: account,
+	}
+	transactions = append(transactions, t)
+	return t
 }
 
 
--- bank_test.go
+++ bank_test.go
@@ -5,26 +5,31 @@ import (
 	"testing"
 )
 
-func TestSetup(t *testing.T) {
+//func TestSetup(t *testing.T) {
+//	accounts = append(accounts,
+//		account{
+//			"Alice",
+//			50,
+//		},
+//		account{
+//			"Daisy",
+//			0,
+//		},
+//		account{
+//			"Rose",
+//			0,
+//		},
+//	)
+//	fmt.Println(accounts)
+//}
+
+// Deposit $50 into Alice's account
+func TestDeposit(t *testing.T) {
 	accounts = append(accounts,
 		account{
 			"Alice",
 			50,
-		},
-		account{
-			"Daisy",
-			0,
-		},
-		account{
-			"Rose",
-			0,
-		},
-	)
-	fmt.Println(accounts)
-}
-
-// Deposit $50 into Alice's account
-func TestDeposit(t *testing.T) {
+	},)
 	account, err := findAccount("Alice")
 	if err != nil {
 		t.Fatal("expected no error got", err)
@@ -35,9 +40,26 @@ func TestDeposit(t *testing.T) {
 	}
 }
 
+func TestWithdraw(t *testing.T) {
+	accounts = append(accounts,
+		account{
+			"Alice",
+			50,
+		},
+	)
+		account, err := findAccount("Alice")
+	if err != nil {
+		t.Fatal("expected no error got", err)
+	}
+	transaction := withdraw(account,40)
+	if transaction.account.balance !=  10 {
+		t.Errorf("got #{transaction.account.balance}; want 10")
+	}
+
+}
+
 func findAccount(customerName string) (account, error) {
 	for i := range accounts {
-		//fmt.Printf("Accounts is %s", accounts[i].customer)
 		if accounts[i].customer == customerName {
 			return accounts[i],nil
 		}
@@ -46,13 +68,25 @@ func findAccount(customerName string) (account, error) {
 	return account{"",0},fmt.Errorf("Account not found")
 }
 
-func TestBankBalance(t *testing.T) {
-	bankBalance := 0
-	bankBalance = findBankBalance(transactions)
-	if bankBalance !=  50 {
-		t.Errorf("got #{bankBalance}; want 50")
-	}
-}
+//func TestBankBalance(t *testing.T) {
+//	accounts = append(accounts,
+//		account{
+//			"Alice",
+//			50,
+//			},
+//		account{
+//			"Daisy",
+//			50,
+//			},
+//		)
+//	bankBalance := 0
+//	bankBalance = findBankBalance(transactions)
+//	if bankBalance !=  100 {
+//		t.Errorf("got #{bankBalance}; want 100")
+//	}
+//}
+
+
 
 
 
